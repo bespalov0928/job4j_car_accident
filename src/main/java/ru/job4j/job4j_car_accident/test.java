5897e6ce21e1788ef588d679eb55e2dcb71360fe
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2021-10-17 19:29:07 +0300

    3.4.0. Проект. Автонарушители

--- src/main/java/ru/job4j/job4j_car_accident/test.java
+++ src/main/java/ru/job4j/job4j_car_accident/test.java
@@ -3,5 +3,6 @@ package ru.job4j.job4j_car_accident;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world1");
     }
 }
