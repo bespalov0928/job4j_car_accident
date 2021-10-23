5fa1e47fed430667c62684b6003757a5d3f0ca96
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2021-10-23 06:34:27 +0300

    3.4.1.1. Spring DI. Запуск

--- src/main/java/ru/job4j/job4j_car_accident/di/SpringDI.java
+++ src/main/java/ru/job4j/job4j_car_accident/di/SpringDI.java
@@ -1,6 +1,11 @@
-package ru.job4j.job4j_car_accident;
+package ru.job4j.job4j_car_accident.di;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-public class Main {
+import ru.job4j.job4j_car_accident.ConsoleInput;
+import ru.job4j.job4j_car_accident.StartUI;
+import ru.job4j.job4j_car_accident.Store;
+
+public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(Store.class);
@@ -11,5 +16,7 @@ public class Main {
         ui.add("Petr Arsentev");
         ui.add("Ivan ivanov");
         ui.print();
+
     }
+
 }
