fc011343de36c1ca6b3b696485f6aab5e7c0057d
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2021-10-23 07:16:48 +0300

    3.4.1.2. Сканирование проекта

--- src/main/java/ru/job4j/job4j_car_accident/test.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.job4j_car_accident;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world1");
-    }
-}
