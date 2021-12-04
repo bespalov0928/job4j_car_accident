44d0997914411879fa224edc7cd0584e3aaf6635
bitcoin <bespalov0928@mail.ru>
2021-12-04 15:37:19 +0700

    3.4.3.2. Spring Data

--- src/main/java/ru/job4j/accident/repository/AccidentRepository.java
+++ src/main/java/ru/job4j/accident/repository/AccidentRepository.java
@@ -1,5 +1,6 @@
 package ru.job4j.accident.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.model.AccidentType;
@@ -8,8 +9,27 @@ import ru.job4j.accident.model.Rule;
 import java.util.List;
 
 public interface AccidentRepository extends CrudRepository<Accident, Integer> {
+
+    @Query("select accident.id as id, accident.name as name, accident.text as text, accident.address as address, accident.type as type ,accidentType.id as typeId, accidentType.name as typeName from accident left join accidentType on accident.type = accidentType.id")
     public List<Accident> findAll();
 
-    public Accident add(Accident accident);
+    public Accident findById(int id);
+
+    public Accident save(Accident accident);
+
+
+    @Query("select id, name from accidentType")
+    public List<AccidentType> findAllAccidentType();
+
+    @Query("select id, name from accidentType where id = ?1")
+    public Accident findByIdType(int id);
+
+
+    @Query("select id, name from rule")
+    public List<Rule> findAllRule();
+
+    @Query("select id, name from rule where id = ?1")
+    public Rule findByIdRule(int id);
+
 
 }
